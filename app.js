76e023ecf176c415638dc8ce4124a4d714e24b2d
Thiago Moraes <thiagomoraesf95@gmail.com>
2021-11-25 23:43:44 -0300

    estrutura do projeto

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+import homeRoutes from './src/routes/homeRoutes';
+import express from 'express';
 
+class App{
+  constructor(){
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares(){
+    this.app.use(express.urlencoded({ extended: true}));
+    this.app.use(express.json());
+  }
+
+  routes(){
+    this.app.use('/', homeRoutes);
+
+  }
+}
+
+export default new App().app;
