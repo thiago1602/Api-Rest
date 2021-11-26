7d3cf024968b3d3424677b781cc2afaa1793aec0
Thiago Moraes <thiagomoraesf95@gmail.com>
2021-11-26 09:55:20 -0300

    config

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
   },
   rules: {
     "no-console": "off",
-    "class-methods-use-this": "off"
+    "class-methods-use-this": "off",
+    "import/first":"of"
   },
 };
--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import homeRoutes from './src/routes/homeRoutes';
 import express from 'express';
 
